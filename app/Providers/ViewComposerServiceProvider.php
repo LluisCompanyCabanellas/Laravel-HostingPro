7d9e118189bd1f68ac56e4ecd64c05c9660d5e45
LluisCompanyCabanellas <lluiscompanycaba@gmail.com>
2022-06-08 15:39:59 +0200

    qqqqqq

---
+++ app/Providers/ViewComposerServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ViewComposerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        view()->composer([
+            'admin.pages.products.index'],//html donde esten disponibles las categorias
+            'App\Http\ViewComposers\Admin\ProductCategories'
+    );
+
+        view()->composer([
+            'front.pages.products.index'],
+            'App\Http\ViewComposers\Front\ProductCategories'
+        );
+
+
+
+    }
+}
